53bd48e753fe08db0dacb45139d77b4886d77048
Florent VICENTE <vicentegroupe@gmail.com>
2022-12-05 22:45:18 +0100

    useEffect for bumping btn

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,16 +1,29 @@
 import styles from "./headerCardButton.module.css"
 import { CartIcon } from "../Cart/CartIcon"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { CartContext } from "../../store/cart-context"
 
 
 export const HeaderCardButton = ({ onClick }) => {
+  const [ btnIsAnimated, setBtnIsAnimated ] = useState(false)
   const cartCtx = useContext(CartContext)
 
+  const { items } = cartCtx
   const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => currNumber + item.amount, 0)
 
+  const btnClasses = `${styles.button} ${btnIsAnimated ? styles.bump : ""}`
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) return
+    setBtnIsAnimated(true)
+
+    const timer = setTimeout(() => setBtnIsAnimated(false), 300)
+
+    return () => clearTimeout(timer)
+  }, [ items ])
+
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={styles.icon}><CartIcon /></span>
       <span>Your Cart</span>
       <span className={styles.badge}>{numberOfCartItems}</span>
